387c46b0b66c739c6c072ecd3636e2510ee953dd
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-01-26 11:53:34 +0100

    Update constants.h

--- tms/constants.h
+++ tms/constants.h
@@ -3,13 +3,14 @@
 
 #define RED_LED_PIN 5
 #define GREEN_LED_PIN 4
-#define ECHO_PIN 3
-#define TRIG_PIN 1
+#define ECHO_PIN 11
+#define TRIG_PIN 12
 #define FREQUENCE 1000 // F = 1 Hz
 
 enum TMSState {
     WORKING,
     NOT_WORKING
 };
+
 
 #endif
