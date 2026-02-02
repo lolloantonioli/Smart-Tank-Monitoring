2ee101a4855acdd8cb26d2a1d9bbe22ec8e3c6e0
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-02-02 12:30:57 +0100

    Update manualInputTask.cpp

--- wcs/tasks/manualInputTask.cpp
+++ wcs/tasks/manualInputTask.cpp
@@ -7,18 +7,29 @@ ManualInputTask::ManualInputTask(Button* btn, Potentiometer* p, Context* ctx) {
     pot = p;
     context = ctx;
     lastButtonState = false;
+    lastPotVal = -1; 
 }
 
 void ManualInputTask::tick() {
-    // Gestione Pulsante
     bool currState = button->isPressed();
     if (currState && !lastButtonState) {
         context->setButtonPressed(true);
     }
     lastButtonState = currState;
 
-    // Gestione Potenziometro
-    int val = pot->getValue();
+    int rawSum = 0;
+    for(int i=0; i<3; i++) {
+        rawSum += pot->getValue();
+    }
+    int val = rawSum / 3;
+    
     int level = map(val, 0, 1023, 0, 100);
-    context->setManualLevel(level);
+    
+    if (abs(level - lastPotVal) >= 5) {
+        context->setManualLevel(level);
+        lastPotVal = level;
+        
+        Serial.print("VALVE:");
+        Serial.println(level);
+    }
 }
