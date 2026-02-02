648384811ee0725edfa0179822310e8156f6eb96
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-02-02 12:27:05 +0100

    Update commTask.cpp

--- wcs/tasks/commTask.cpp
+++ wcs/tasks/commTask.cpp
@@ -11,19 +11,31 @@ void CommTask::tick() {
         msg.trim();
         if (msg.startsWith("VALVE:")) {
             int val = msg.substring(6).toInt();
-            context->setRemoteAutoLevel(val);
+            if (context->getSystemState() == MANUAL) {
+                context->setManualLevel(val);
+            } else {
+                context->setRemoteAutoLevel(val);
+            }
+            
         } else if (msg.equals("CMD:MANUAL")) {
             context->setSystemState(MANUAL);
         } else if (msg.equals("CMD:AUTO")) {
             context->setSystemState(AUTOMATIC);
+        } else if (msg.equals("CMD:UNCONNECTED")) {
+            context->setSystemState(UNCONNECTED);
         }
     }
+    
     WCSState currentState = context->getSystemState();
     if (currentState != lastSentState) {
         if (currentState == MANUAL) {
             Serial.println("MOD:MANUAL");
-        } else if (currentState == AUTOMATIC) {
+        }
+        else if (currentState == AUTOMATIC) {
             Serial.println("MOD:AUTO");
+        }
+        else if (currentState == UNCONNECTED) {
+            Serial.println("MOD:UNCONNECTED");
         }
         lastSentState = currentState;
     }
